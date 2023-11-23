cc9a8c745b882ede99b32e4577b6c7b5a3112fac
dothanhtruc <trucdo0411@gmail.com>
2023-11-23 23:42:23 +0700

    60. Lấy Danh Sách Users và Xóa Users Với Redux - READ, DELETE

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -6,6 +6,7 @@ import * as actions from "../../../store/actions";
 import './UserRedux.scss';
 import Lightbox from "react-image-lightbox";
 import 'react-image-lightbox/style.css';
+import TableManageUser from "./TableManageUser";
 
 class UserRedux extends Component {
     constructor(props) {
@@ -62,6 +63,22 @@ class UserRedux extends Component {
                 position: arrPositions && arrPositions.length > 0 ? arrPositions[0].key : ''
             })
         }
+
+        if (prevProps.listUsers !== this.props.listUsers) {
+            this.setState({
+                email: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+                phoneNumber: '',
+                address: '',
+                gender: '',
+                position: '',
+                role: '',
+                avatar: '',
+            })
+        }
+
     }
 
     handleOnchangeImage = (event) => {
@@ -244,8 +261,12 @@ class UserRedux extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-12 mt-4">
+                            <div className="col-12 my-3">
                                 <button className="btn btn-primary" onClick={() => this.handleSaveUser()}><FormattedMessage id='manage-user.save' /></button>
+                            </div>
+
+                            <div className="col-12 mb-5">
+                                <TableManageUser />
                             </div>
                         </div>
                     </div>
@@ -270,6 +291,7 @@ const mapStateToProps = state => {
         roleRedux: state.admin.roles,
         positionRedux: state.admin.positions,
         isLoadingGender: state.admin.isLoadingGender,
+        listUsers: state.admin.users
     }
 }
 
@@ -278,7 +300,8 @@ const mapDispatchToProps = dispatch => {
         getGenderStart: () => dispatch(actions.fetchGenderStart()),
         getPositionStart: () => dispatch(actions.fetchPositionStart()),
         getRoleStart: () => dispatch(actions.fetchRoleStart()),
-        createNewUser: (data) => dispatch(actions.createNewUser(data))
+        createNewUser: (data) => dispatch(actions.createNewUser(data)),
+        fetchUserRedux: () => dispatch(actions.fetchAllUsersStart())
     }
 }
 
--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,5 +1,7 @@
 import actionTypes from "./actionTypes";
-import { getAllCodeService, createNewUserService } from "../../services/userService";
+import { getAllCodeService, createNewUserService, getAllUsers, deleteUserService } from "../../services/userService";
+import { toast } from "react-toastify";
+import { dispatch } from "../../redux";
 
 export const fetchGenderStart = () => {
     return async (dispatch, getState) => {
@@ -83,9 +85,10 @@ export const createNewUser = (data) => {
     return async (dispatch, getState) => {
         try {
             let res = await createNewUserService(data);
-            console.log('dothanhtruc check create user redux: ', res)
             if (res && res.errCode === 0) {
+                toast.success('Create a new user succeed!')
                 dispatch(saveUserSuccess())
+                dispatch(fetchAllUsersStart())
             } else {
                 dispatch(saveUserFailed())
             }
@@ -96,10 +99,67 @@ export const createNewUser = (data) => {
     }
 }
 
-export const saveUserFailed = () => ({
-    type: 'CREATE_USER_FAILDED'
+export const saveUserSuccess = () => ({
+    type: actionTypes.CREATE_USER_SUCCESS
 })
 
-export const saveUserSuccess = () => ({
-    type: 'CREATE_USER_SUCCESS'
+
+export const saveUserFailed = () => ({
+    type: actionTypes.CREATE_USER_FAILDED
+})
+
+export const fetchAllUsersStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllUsers('ALL')
+            if (res && res.errCode === 0) {
+                dispatch(fetchAllUsersSuccess(res.users.reverse()))
+            } else {
+                toast.error('Fetch all users error!')
+                dispatch(fetchAllUsersFailed())
+            }
+        } catch (e) {
+            toast.error('Fetch all users error!')
+
+            dispatch(fetchAllUsersFailed())
+            console.log('fetchAllUsersFailed error', e)
+        }
+    }
+}
+
+export const fetchAllUsersSuccess = (data) => ({
+    type: actionTypes.FETCH_ALL_USERS_SUCCESS,
+    users: data
+})
+
+export const fetchAllUsersFailed = () => ({
+    type: actionTypes.FETCH_ALL_USERS_FAILDED,
+})
+
+export const deleteAUser = (userId) => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await deleteUserService(userId)
+            if (res && res.errCode === 0) {
+                toast.success('Delete the user succeed!')
+                dispatch(deleteUserSuccess())
+                dispatch(fetchAllUsersStart())
+            } else {
+                toast.error('Delete the user error!')
+                dispatch(deleteUserFailed())
+            }
+        } catch (e) {
+            toast.error('Delete the user error!')
+            dispatch(deleteUserFailed())
+            console.log('saveUserFailed error', e)
+        }
+    }
+}
+
+export const deleteUserSuccess = () => ({
+    type: actionTypes.DELETE_USER_SUCCESS
+})
+
+export const deleteUserFailed = () => ({
+    type: actionTypes.DELETE_USER_FAILDED
 })
